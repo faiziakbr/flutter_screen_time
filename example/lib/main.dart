32c86cb4baf39918a80bb3ff615967ef7d1eea67
faizan <faizi.akbr@gmail.com>
2026-04-25 02:48:45 +0500

    package name updated

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_screen_time/flutter_screen_time.dart';
+import 'package:flutter_screen_time_api/flutter_screen_time.dart';
 
 void main() {
   runApp(const MyApp());
